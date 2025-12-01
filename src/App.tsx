c381fc1f57c3b19f99843356325e6a8a6be62d49
chamathkadinusahani3 <chamathkadinusahani3@gmail.com>
2025-12-01 20:23:32 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { WhatsAppButton } from './components/WhatsAppButton';
+import { HomePage } from './pages/HomePage';
+import { TyresPage } from './pages/TyresPage';
+import { ServicesPage } from './pages/ServicesPage';
+import { BrandsPage } from './pages/BrandsPage';
+import { EventsPage } from './pages/EventsPage';
+import { ContactPage } from './pages/ContactPage';
+export function App() {
+  return <BrowserRouter>
+      <div className="min-h-screen bg-black flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/tyres" element={<TyresPage />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/brands" element={<BrandsPage />} />
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+        <WhatsAppButton />
+      </div>
+    </BrowserRouter>;
+}
